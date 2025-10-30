2e35413f4627aa28d207b6a044c9e1a8853e5b14
swaraa19 <swarajatale@gmail.com>
2025-10-30 23:14:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+
+app.post("/reserve", (req, res) => {
+  const data = req.body;
+  let reservations = [];
+
+  if (fs.existsSync("reservations.json")) {
+    reservations = JSON.parse(fs.readFileSync("reservations.json"));
+  }
+
+  reservations.push(data);
+  fs.writeFileSync("reservations.json", JSON.stringify(reservations, null, 2));
+
+  res.json({ message: "Reservation received successfully!" });
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
 
